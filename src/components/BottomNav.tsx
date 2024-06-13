a02106095752f5181aa26a266c901197094e199b
BasavarajMuttagi <basavaraj2770@gmail.com>
2024-06-13 19:38:25 +0530

    create bottom nav

--- src/components/BottomNav.tsx
+++ src/components/BottomNav.tsx
@@ -7,17 +7,17 @@ import {
 const BottomNav = () => {
   return (
     <nav>
-      <ul className="flex items-center justify-between">
-        <li className="px-7 py-4  hover:bg-deep-lagoon-blue hover:text-white hover:rounded-full">
+      <ul className="flex items-center justify-between bg-slate-700">
+        <li className="px-7 py-5 text-gray-400 hover:bg-deep-lagoon-blue hover:text-white hover:rounded-[27px]">
           <House size={22} />
         </li>
-        <li className="px-7 py-4  hover:bg-deep-lagoon-blue hover:text-white hover:rounded-full">
+        <li className="px-7 py-5 text-gray-400 hover:bg-deep-lagoon-blue hover:text-white hover:rounded-[27px]">
           <HeartStraight size={22} />
         </li>
-        <li className="px-7 py-4  hover:bg-deep-lagoon-blue hover:text-white hover:rounded-full">
+        <li className="px-7 py-5 text-gray-400 hover:bg-deep-lagoon-blue hover:text-white hover:rounded-[27px]">
           <BookmarksSimple size={22} />
         </li>
-        <li className="px-7 py-4  hover:bg-deep-lagoon-blue hover:text-white hover:rounded-full">
+        <li className="px-7 py-5 text-gray-400 hover:bg-deep-lagoon-blue hover:text-white hover:rounded-[27px]">
           <User size={22} />
         </li>
       </ul>
