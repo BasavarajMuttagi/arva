f71ba14b534bf48a7bafc80421ca0f7c1a98cd85
BasavarajMuttagi <basavaraj2770@gmail.com>
2024-06-17 02:04:57 +0530

    fix width

--- src/components/ItemCard.tsx
+++ src/components/ItemCard.tsx
@@ -14,15 +14,15 @@ const ItemCard = ({
 }) => {
   return (
     <div className="relative">
-      <div className="bg-seafoam-100 flex items-center p-5 rounded-2xl space-x-4">
+      <div className="bg-seafoam-100 flex items-center  rounded-2xl space-x-4 h-36 p-4">
         <img
           src="https://media.istockphoto.com/id/664313320/photo/espresso-coffee-cup-with-beans-on-vintage-table.jpg?s=612x612&w=0&k=20&c=kaF8P4KuAlVhAm9zNcq5DxSimOv8w3yVQynS4dwPBHc="
           alt="coffee"
-          className="aspect-square rounded-xl h-fit max-h-32 object-cover object-center w-1/2"
+          className="rounded-xl   w-1/3 h-full object-cover object-center"
         />
-        <div className="text-deep-lagoon-blue space-y-3 w-1/2">
+        <div className="text-deep-lagoon-blue  w-2/3 flex flex-col justify-between h-full">
           <p className="font-bold text-base">{name}</p>
-          <p className="text-sm font-normal">{description}</p>
+          <p className="text-sm font-medium">{description}</p>
           <p className="text-sm font-semibold">₹{price.toFixed(2)}</p>
         </div>
       </div>
