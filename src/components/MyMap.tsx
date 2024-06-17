a8e7dca20e7f5afc1780d38dc2718ce975b33165
BasavarajMuttagi <basavaraj2770@gmail.com>
2024-06-17 23:17:11 +0530

    create map with marker

--- src/components/MyMap.tsx
+++ src/components/MyMap.tsx
@@ -1,14 +1,45 @@
 import "leaflet/dist/leaflet.css";
 import { MapContainer, Marker, Popup, TileLayer } from "react-leaflet";
 import useGeoLocation from "../hooks/useGeoLocation";
+import { useEffect, useRef, useState } from "react";
 
 const MyMap = () => {
   const { position } = useGeoLocation();
 
+  const [markerPosition, setMarkerPosition] = useState<[number, number]>([
+    position.latitude,
+    position.longitude,
+  ]);
+  const mapRef = useRef<any>();
+
+  useEffect(() => {
+    const map = mapRef.current;
+    if (!map) return;
+    setTimeout(() => {
+      map.flyTo([markerPosition[0], markerPosition[1]], 15, { duration: 3 });
+    }, 1000);
+  }, [markerPosition]);
+
+  function DraggableMarker() {
+    return (
+      <Marker
+        position={markerPosition}
+        draggable={true}
+        eventHandlers={{
+          dragend: (e) => {
+            console.log(e);
+            setMarkerPosition([e.target._latlng.lat, e.target._latlng.lng]);
+          },
+        }}
+      />
+    );
+  }
+
   return (
     <div className="h-52 rounded p-1">
       <MapContainer
-        center={[position.latitude, position.longitude]}
+        ref={mapRef}
+        center={markerPosition}
         zoom={15}
         scrollWheelZoom={false}
         className="h-full w-full"
@@ -17,9 +48,8 @@ const MyMap = () => {
           attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
           url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
         />
-        <Marker position={[position.latitude, position.longitude]}>
-          <Popup>{[position.latitude, position.longitude]}</Popup>
-        </Marker>
+        <DraggableMarker />
+        <Popup position={markerPosition}>{markerPosition.join(", ")}</Popup>
       </MapContainer>
     </div>
   );
