b6c38b4ab3e48ad94a6c0e9e2f94b353972501c1
BasavarajMuttagi <basavaraj2770@gmail.com>
2024-06-20 00:03:45 +0530

    add motion to payment status

--- src/components/PaymentSuccess.tsx
+++ src/components/PaymentSuccess.tsx
@@ -1,8 +1,39 @@
+import { motion } from "framer-motion";
+import { useEffect, useState } from "react";
+import { useNavigate } from "react-router-dom";
+
 const PaymentSuccess = () => {
+  const navigate = useNavigate();
+  const [count, setCount] = useState(5);
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setCount((prevCount) => prevCount - 1);
+    }, 1000);
+
+    const timer = setTimeout(() => {
+      navigate("/user/orders");
+      clearInterval(interval);
+    }, 5000);
+
+    return () => {
+      clearTimeout(timer);
+      clearInterval(interval);
+    };
+  }, [navigate]);
+
   return (
-    <div className="h-screen bg-green-400 flex justify-center items-center">
-      <div className="text-lg text-white">Payment Success</div>
-    </div>
+    <motion.div
+      initial={{ opacity: 0, y: "-5%" }}
+      animate={{ opacity: 1, y: "0%" }}
+      transition={{ duration: 0.6 }}
+      className="flex h-screen items-center justify-center bg-green-400"
+    >
+      <div className="flex items-center space-x-2 text-xl font-bold text-white">
+        <div>Payment Success</div>
+        <div className="text-lg text-blue-500">{count}</div>
+      </div>
+    </motion.div>
   );
 };
 
